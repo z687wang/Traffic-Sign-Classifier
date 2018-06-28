3ad546e55ff89b651d6b9e84f8b8307ddf9ccfc5
ZHE WANG <z687wang@edu.uwaterloo.ca>
2018-06-28 21:52:15 +0800

    add sign names list

--- src/data_preprocess/load.py
+++ src/data_preprocess/load.py
@@ -1,12 +1,18 @@
 import pickle
 import numpy as np
+import random
+from matplotlib import pyplot as plt
+import csv
 
 class TrainingData:
     def __init__(self, training_file='../../data/train.p', validation_file='../../data/valid.p',
-                  testing_file='../../data/test.p'):
+                  testing_file='../../data/test.p', name_file='../../data/signnames.csv'):
         self.training_file = training_file
         self.validation_file = validation_file
         self.testing_file = testing_file
+        self.name_file = name_file
+        self.name_list = np.genfromtxt(self.name_file, skip_header=1, dtype=[('myint', 'i8'), ('mysring', 'S55')],
+                                    delimiter=',')
         self.load_data(self.training_file, self.validation_file, self.testing_file)
 
     def load_data(self, training_file='../../data/train.p', validation_file='../../data/valid.p',
@@ -39,6 +45,39 @@ class TrainingData:
         print("Image data shape =", img_shape)
         print("Number of classes =", n_classes)
 
+    def print_distribute(self):
+        unique_train, counts_train = np.unique(self.y_train, return_counts=True)
+        plt.bar(unique_train, counts_train)
+        plt.grid()
+        plt.title("Train Dataset Sign Counts")
+        plt.savefig('train_sign_counts.png')
+
+        unique_test, counts_test = np.unique(self.y_test, return_counts=True)
+        plt.bar(unique_test, counts_test)
+        plt.grid()
+        plt.title("Test Dataset Sign Counts")
+        plt.savefig('test_sign_counts.png')
+
+        unique_valid, counts_valid = np.unique(self.y_valid, return_counts=True)
+        plt.bar(unique_valid, counts_valid)
+        plt.grid()
+        plt.title("Valid Dataset Sign Counts")
+        plt.savefig('valid_sign_counts.png')
+
+    def print_examples(self):
+        fig, axs = plt.subplots(4, 5, figsize=(15, 6))
+        fig.subplots_adjust(hspace=.2, wspace=.001)
+        axs = axs.ravel()
+        for i in range(20):
+            index = random.randint(0, len(self.X_train))
+            image = self.X_train[index]
+            axs[i].axis('off')
+            axs[i].imshow(image)
+            axs[i].set_title(self.name_list[self.y_train[index]][1].decode('ascii'), fontsize=10)
+        plt.savefig('sign_examples')
+
 if __name__ =='__main__':
     train_data = TrainingData()
     train_data.print_stats()
+    train_data.print_distribute()
+    train_data.print_examples()
